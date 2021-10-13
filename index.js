68966b9a13a85fc140dd885cf2d4ebe9bd3b3376
qqyh <2107805487@qq.com>
2021-10-13 14:49:44 +0800

    添加了一个onload事件

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-var tb = document.querySelector('.tb');
-        var as = tb.querySelectorAll("a");
-        var boxs = document.querySelectorAll(".box");
-        for (var i = 0; i < as.length; i++) {
-            as[i].setAttribute("index", i);
-            as[i].onclick = function () {
-                for (var i = 0; i < as.length; i++) {
-                    as[i].className = '';
-                }
-                this.className = 'active';
-                var index = this.getAttribute("index");
-                for (var i = 0; i < boxs.length; i++) {
-                    boxs[i].style.display = 'none';
-                }
-                boxs[index].style.display = 'block';
+window.onload = function () {
+    var tb = document.querySelector('.tb');
+    var as = tb.querySelectorAll("a");
+    var boxs = document.querySelectorAll(".box");
+    for (var i = 0; i < as.length; i++) {
+        as[i].setAttribute("index", i);
+        as[i].onclick = function () {
+            for (var i = 0; i < as.length; i++) {
+                as[i].className = '';
             }
+            this.className = 'active';
+            var index = this.getAttribute("index");
+            for (var i = 0; i < boxs.length; i++) {
+                boxs[i].style.display = 'none';
+            }
+            boxs[index].style.display = 'block';
         }
+    }
+}
